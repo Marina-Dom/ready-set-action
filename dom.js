46ae8927c489c01bc012865372c3f18cf40e7abc
marina <marina-dominguez@alumni.nmu.edu>
2022-07-29 08:46:56 -0400

    first commit

---
+++ dom.js
@@ -0,0 +1,27 @@
+function main(){
+    const growMe = document.getElementById("grow-me");
+    growMe.classList.add("big");
+
+    const shrinkMe = document.getElementById("shrink-me");
+    shrinkMe.classList.remove("big");
+
+    document.getElementById("list");
+    console.log("list");
+
+    // const list = document.querySelectorAll("li");
+    // console.log(list);
+
+    const hide = document.getElementById("hide-me");
+    hide.style.display = "none";
+
+    const show = document.getElementById("show-me");
+    show.style.display = "block";
+
+}
+
+
+
+//declare variable
+//query select all
+//loop over array
+
